5dd5d01a07a57d6292a881799db94330bbee4834
Dennis Reedy <dennis.reedy@gmail.com>
2010-12-20 08:53:06 -0500

    Move version to 4.1

--- boot/src/main/java/org/rioproject/RioVersion.java
+++ boot/src/main/java/org/rioproject/RioVersion.java
@@ -27,7 +27,7 @@ import java.net.URL;
  */
 public class RioVersion {
     /** Current version of Rio */ 
-    public static final String VERSION = "4.1-SNAPSHOT";
+    public static final String VERSION = "4.1";
 
     /**
      * Get the build number.
