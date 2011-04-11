ab4064a6ca9eab95b7d0016c4a8b83bd7dae26a8
dreedyman <dennis.reedy@gmail.com>
2011-04-11 11:54:13 -0400

    RIO-302: applied patch and added test case

--- plugins/maven-rio-plugin/src/main/java/org/rioproject/tools/maven/ClassDepAndJarMojo.java
+++ plugins/maven-rio-plugin/src/main/java/org/rioproject/tools/maven/ClassDepAndJarMojo.java
@@ -1,5 +1,17 @@
 /*
- * Copyright (c) 2007, Kalixia, SARL. All Rights Reserved.
+ * Copyright to the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  */
 package org.rioproject.tools.maven;
 
@@ -208,6 +220,20 @@ public class ClassDepAndJarMojo extends AbstractMojo {
         return jarArchiver;
     }
 
+    protected void parseDependenciesAndBuildClasspath(Path classpath) {
+        for (Object dependency : getDependencies()) {
+            Artifact artifact = (Artifact) dependency;
+            if ("pom".equalsIgnoreCase(artifact.getType())) {
+                getLog().debug("Skipping pom artifact element " + artifact.getFile());
+                continue;
+            }
+            getLog().debug("Found artifact element " + artifact.getFile());
+            if (artifact.getFile() != null)
+                classpath.append(new Path(antProject,
+                                          artifact.getFile().toString()));
+        }
+    }
+
     protected void createJar(CreateJar createJar,
                              Project antProject,
                              String outputDir,
@@ -256,13 +282,19 @@ public class ClassDepAndJarMojo extends AbstractMojo {
             getLog().debug("Found classpath element " + o.toString());
             classpath.append(new Path(antProject, o.toString()));
         }
-        for (Object dependency : getDependencies()) {
+        /*for (Object dependency : getDependencies()) {
             Artifact artifact = (Artifact) dependency;
+            if ("pom".equalsIgnoreCase(artifact.getType())) {
+                getLog().debug("Skipping pom artifact element " + artifact.getFile());
+                continue;
+            }
             getLog().debug("Found artifact element " + artifact.getFile());
             if (artifact.getFile() != null)
                 classpath.append(new Path(antProject,
                                           artifact.getFile().toString()));
-        }
+        }*/
+        parseDependenciesAndBuildClasspath(classpath);
+
         getLog().debug("Build ant path: " + classpath.toString());
         getLog().info("Building jar: " + jarFileName);
 
