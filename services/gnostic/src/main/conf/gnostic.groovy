fd18d3505ba77f33d6fc1cc1a2384d66091b3c93
dreedyman <dennis.reedy@gmail.com>
2011-04-26 10:51:35 -0400

    bump version to 4.3-SNAPSHOT

--- services/gnostic/src/main/conf/gnostic.groovy
+++ services/gnostic/src/main/conf/gnostic.groovy
@@ -4,8 +4,8 @@ import java.util.logging.Level
 deployment(name: 'Gnostic') {
     groups System.getProperty(Constants.GROUPS_PROPERTY_NAME, System.getProperty('user.name'))
 
-    artifact id: 'service', 'org.rioproject:gnostic:4.2'
-    artifact id: 'service-dl', 'org.rioproject:gnostic:dl:4.2'
+    artifact id: 'service', 'org.rioproject:gnostic:4.3-SNAPSHOT'
+    artifact id: 'service-dl', 'org.rioproject:gnostic:dl:4.3-SNAPSHOT'
 
     logging {
         logger 'org.rioproject.gnostic', Level.FINE
