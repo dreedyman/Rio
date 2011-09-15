5fe4632679ca054819cfff902c340e8f0f8cb48c
dreedyman <dennis.reedy@gmail.com>
2011-09-15 13:58:31 -0400

    Working on tests

---
+++ rio-test/src/test/java/org/rioproject/test/rmi/ResolvingLoaderTest.java
@@ -0,0 +1,77 @@
+/*
+ * Copyright to the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.rioproject.test.rmi;
+
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import org.rioproject.RioVersion;
+import org.rioproject.rmi.ResolvingLoader;
+import org.rioproject.url.artifact.ArtifactURLStreamHandlerFactory;
+
+import java.io.File;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.net.URLClassLoader;
+import java.rmi.RMISecurityManager;
+
+/**
+ * Tests the {@link org.rioproject.rmi.ResolvingLoader}
+ */
+public class ResolvingLoaderTest {
+    @BeforeClass
+    public static void setSecurityPolicy() {
+        URL.setURLStreamHandlerFactory(new ArtifactURLStreamHandlerFactory());
+        String rioHome = System.getProperty("RIO_HOME");
+        assertNotNull(rioHome);
+        StringBuilder sb = new StringBuilder();
+        sb.append(rioHome).append(File.separator).append("policy").append(File.separator).append("policy.all");
+        System.setProperty("java.security.policy", sb.toString());
+        if (System.getSecurityManager() == null)
+            System.setSecurityManager(new RMISecurityManager());
+    }
+
+    @Test
+    public void testCreation() {
+        ResolvingLoader loader = new ResolvingLoader();
+        assertNotNull(loader);
+    }
+
+    @Test
+    public void testGetClassLoader() throws MalformedURLException, ClassNotFoundException {
+        ResolvingLoader loader = new ResolvingLoader();
+        assertNotNull(loader);
+        ClassLoader sysCL = ClassLoader.getSystemClassLoader();
+        ClassLoader classLoader = loader.getClassLoader("artifact:com.sun.jini/reggie/jar/dl/2.1;http://www.rio-project.org/maven2");
+        assertNotNull(classLoader);
+        assertTrue("Returned ClassLoader should not be the same as the system ClassLoader", !(classLoader.equals(sysCL)));
+        Class c = classLoader.loadClass("com.sun.jini.reggie.ConstrainableAdminProxy");
+        assertNotNull(c);
+    }
+
+    @Test
+    public void testGetClassAnnotation() throws MalformedURLException, ClassNotFoundException {
+        URL[] u = new URL[]{new URL("artifact:com.sun.jini/reggie/jar/dl/2.1;http://www.rio-project.org/maven2")};
+        URLClassLoader cl = new URLClassLoader(u);
+        ResolvingLoader loader = new ResolvingLoader();
+        Class<?> c = cl.loadClass("com.sun.jini.reggie.ConstrainableAdminProxy");
+        String annotation = loader.getClassAnnotation(c);
+        assertNotNull(annotation);
+    }
+
+}
