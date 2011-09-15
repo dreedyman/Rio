026089ce8887a0e61b9c6d4369d72ebcdd80606c
dreedyman <dennis.reedy@gmail.com>
2011-09-15 16:07:09 -0400

    Moved to rio-platform

--- boot/src/test/resources/bad_config.groovy
+++
@@ -1,8 +0,0 @@
-import org.rioproject.config.Component
-
-/* Use a reserved Java keyword for a component name, will result in an
- * IllegalArgumentException */
-@Component('finally')
-public class bad_config {
-
-}
