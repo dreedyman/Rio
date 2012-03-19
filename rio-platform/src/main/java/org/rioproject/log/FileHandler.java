31b31f9885fcbc14f80f6205736df0112e78d278
dreedyman <dennis.reedy@gmail.com>
2012-03-19 12:30:25 -0400

    Added a custom extension for the FileHandler, allowing the name of the file the FileHandler creates to be derived from runtime settings and system properties

---
+++ rio-platform/src/main/java/org/rioproject/log/FileHandler.java
@@ -0,0 +1,143 @@
+/*
+ * Copyright to the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.rioproject.log;
+
+import java.io.*;
+import java.util.logging.*;
+
+/**
+ * An extension of {@link java.util.logging.FileHandler} that sets the pattern based on system properties.
+ *
+ * <b>Configuration:</b>
+ * By default each <tt>FileHandler</tt> is initialized using the following
+ * <tt>LogManager</tt> configuration properties.  If properties are not defined
+ * (or have invalid values) then the specified default values are used.
+ * <ul>
+ * <li>org.rioproject.log.FileHandler.level specifies the default level for the <tt>Handler</tt>
+ *	  (defaults to <tt>Level.ALL</tt>).
+ * <li>org.rioproject.log.FileHandler.filter specifies the name of a <tt>Filter</tt> class to use
+ *	  (defaults to no <tt>Filter</tt>).
+ * <li>org.rioproject.log.FileHandler.formatter specifies the name of a <tt>Formatter</tt> class to use
+ *    (defaults to <tt>org.rioproject.log.XMLFormatter</tt>)
+ * <li>org.rioproject.log.FileHandler.encoding the name of the character set encoding to use
+ *    (defaults to the default platform encoding).
+ * <li>org.rioproject.log.FileHandler.limit specifies an approximate maximum amount to write (in bytes)
+ * 	  to any one file.  If this is zero, then there is no limit. (Defaults to no limit).
+ * <li>org.rioproject.log.FileHandler.count specifies how many output files to cycle through (defaults to 1).
+ * <li>org.rioproject.log.FileHandler.pattern specifies a pattern for generating the output file name.  See
+ *     below for details. (Defaults to "%h/java%u.log").
+ * <li>org.rioproject.log.FileHandler.append specifies whether the FileHandler should append onto
+ *     any existing files (defaults to false).
+ * </ul>
+ * <p>
+ */
+public class FileHandler extends java.util.logging.FileHandler {
+    static Configuration configuration = getConfiguration();
+
+    public FileHandler() throws IOException, SecurityException {
+        super(configuration.pattern, configuration.limit, configuration.count, configuration.append);
+        setEncoding(configuration.encoding);
+    }
+
+    @Override
+    public void setFormatter(Formatter formatter) throws SecurityException {
+        super.setFormatter(formatter);
+        /* if we have a RioLogFormatter, turn of colorization */
+        if(formatter instanceof RioLogFormatter) {
+            ((RioLogFormatter)formatter).setColorization(false);
+        }
+    }
+
+    private static Configuration getConfiguration() {
+        LogManager manager = LogManager.getLogManager();
+        String propertyBase = FileHandler.class.getName();
+        String pattern = manager.getProperty(propertyBase+".pattern");
+        int limit = getIntProperty(manager, propertyBase + ".limit", 0);
+        if (limit < 0) {
+            limit = 0;
+        }
+        int count = getIntProperty(manager, propertyBase + ".count", 0);
+        if (count <= 0) {
+            count = 1;
+        }
+        boolean append = getBooleanProperty(manager, propertyBase + ".append", false);
+        String encoding = manager.getProperty(propertyBase+".encoding");
+        String logDirectory = System.getProperty("RIO_LOG_DIR");
+        String service = System.getProperty("org.rioproject.service");
+        if(logDirectory!=null)
+            pattern = replace(pattern, "${RIO_LOG_DIR}", logDirectory);        
+        if(service!=null)
+            pattern = replace(pattern, "${org.rioproject.service}", service);
+
+        return new Configuration(limit, count, append, pattern, encoding);
+    }
+
+    static int getIntProperty(LogManager manager, String name, int defaultValue) {
+        String val = manager.getProperty(name);
+        if (val == null) {
+            return defaultValue;
+        }
+        try {
+            return Integer.parseInt(val.trim());
+        } catch (Exception ex) {
+            return defaultValue;
+        }
+    }
+
+    static boolean getBooleanProperty(LogManager manager, String name, boolean defaultValue) {
+        String val = manager.getProperty(name);
+        if (val == null) {
+            return defaultValue;
+        }
+        val = val.toLowerCase();
+        if (val.equals("true") || val.equals("1")) {
+            return true;
+        } else if (val.equals("false") || val.equals("0")) {
+            return false;
+        }
+        return defaultValue;
+    }
+
+    static String replace(String str, String pattern, String replace) {
+        int s = 0;
+        int e;
+        StringBuilder result = new StringBuilder();
+
+        while ((e = str.indexOf(pattern, s)) >= 0) {
+            result.append(str.substring(s, e));
+            result.append(replace);
+            s = e + pattern.length();
+        }
+        result.append(str.substring(s));
+        return result.toString();
+    }
+    
+    private static class Configuration {
+        int limit;
+        int count;
+        boolean append;
+        String pattern;
+        String encoding;
+
+        private Configuration(int limit, int count, boolean append, String pattern, String encoding) {
+            this.limit = limit;
+            this.count = count;
+            this.append = append;
+            this.pattern = pattern;
+            this.encoding = encoding;
+        }
+    }
+}
