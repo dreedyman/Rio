88d8e8f9fa80d861720e59ae963ad18668c1184b
dreedyman <dennis.reedy@gmail.com>
2012-03-28 18:13:13 -0400

    Modify monitor configuration to keep it more in line with standard product setup

--- rio-test/src/test/conf/start-monitor.groovy
+++ rio-test/src/test/conf/start-monitor.groovy
@@ -6,31 +6,33 @@
 import org.rioproject.config.Component
 
 import org.rioproject.boot.ServiceDescriptorUtil;
-import com.sun.jini.start.ServiceDescriptor;
+import com.sun.jini.start.ServiceDescriptor
+import org.rioproject.resolver.maven2.Repository;
 
 @Component('com.sun.jini.start')
 class StartMonitorConfig {
 
+    String[] getMonitorConfigArgs(String rioHome) {
+        def configArgs = [rioHome+'/config/common.groovy', rioHome+'/config/monitor.groovy']
+        return configArgs as String[]
+    }
+
     ServiceDescriptor[] getServiceDescriptors() {
-        String m2Home = "${System.getProperty("user.home")}/.m2"
+        String m2Repo = Repository.getLocalRepository().absolutePath
         String rioHome = System.getProperty('RIO_HOME')
 
         def websterRoots = [rioHome+'/lib-dl', ';',
-                            rioHome+'/lib',     ';',
-                            rioHome+'/lib',
-                            m2Home+'/repository']
+                rioHome+'/lib',    ';',
+                rioHome+'/deploy', ';',
+                m2Repo]
 
         String policyFile = rioHome+'/policy/policy.all'
-        def monitorConfigs = [rioHome+'/config/common.groovy', rioHome+'/config/monitor.groovy']
-        //String reggieConfig = rioHome+'/config/reggie.groovy'
 
         def serviceDescriptors = [
-            ServiceDescriptorUtil.getWebster(policyFile, '0', websterRoots as String[]),
-            //ServiceDescriptorUtil.getLookup(policyFile, reggieConfig),
-            ServiceDescriptorUtil.getMonitor(policyFile, monitorConfigs  as String[])
+                ServiceDescriptorUtil.getWebster(policyFile, '0', websterRoots as String[]),
+                ServiceDescriptorUtil.getMonitor(policyFile, getMonitorConfigArgs(rioHome))
         ]
 
         return (ServiceDescriptor[])serviceDescriptors
     }
-
 }
