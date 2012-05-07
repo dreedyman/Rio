15c67d0b73831373e90ec00f57006775071d4927
dreedyman <dennis.reedy@gmail.com>
2012-05-07 13:46:43 -0400

    Dont use overridable methods in Webster creation

--- rio-lib/src/main/java/org/rioproject/resources/util/JavaEntityResolver.java
+++ rio-lib/src/main/java/org/rioproject/resources/util/JavaEntityResolver.java
@@ -40,8 +40,7 @@ public class JavaEntityResolver implements EntityResolver {
     /** Holds value of property classLoader. */
     private ClassLoader classLoader;
     /** Logger instance  */
-    private final static Logger logger =
-        Logger.getLogger("org.rioproject.resources.util");
+    private final static Logger logger = Logger.getLogger("org.rioproject.resources.util");
     static final String OLD_DTD = "org/jini/rio/dtd/rio_opstring.dtd";
     static final String DTD = "org/rioproject/dtd/rio_opstring.dtd";
     static final String PUBLIC_DTD = "http://www.rio-project.org/dtd/rio_opstring.dtd";
@@ -54,120 +53,117 @@ public class JavaEntityResolver implements EntityResolver {
 
     public JavaEntityResolver(ClassLoader classLoader) {
         this();
-        setClassLoader(classLoader);
+        this.classLoader = classLoader;
     }
 
     @SuppressWarnings("unchecked")
-    public InputSource resolveEntity(String publicId, String systemId) 
-    throws SAXException, IOException {
+    public InputSource resolveEntity(final String publicId, final String systemIdentifier)
+        throws SAXException, IOException {
         if(publicId != null) {
             if(logger.isLoggable(Level.FINE)) {
                 logger.log(Level.FINE,
-                           "Looking for public {0}, "+
-                           "java:// for public is not yet implemented",
+                           "Looking for public {0}, java:// for public is not yet implemented",
                            new Object[] {publicId});
-                
+
             }
             System.out.flush();
             // not yet implemented.
         }
         boolean isThrowing = false;
-        if(systemId != null) {
-            if(systemId.equals(PUBLIC_DTD))
-                systemId = "java://"+DTD;
-            if(systemId.startsWith("java://")) {
-                String target = systemId.substring(7);
-                if (OLD_DTD.equals(target))
-                    target = DTD;
-                int paren;
-                if((paren = target.indexOf('(')) > 2) { 
-                    /* we have a java class to execute
-                     * the form foo.bar() is executed and the result is passed back 
-                     * as a string */
-                    int demarc = target.lastIndexOf('.', paren);
-                    String classpart = target.substring(0, demarc);
-                    Hashtable methodTable = 
-                        getMethodParts(target.substring(demarc+1));
-                    List list = new LinkedList();
-                    Class[] paramTypes;
-                    Object[] paramArgs=null;
-                    try {
-                        Class clazz = Class.forName(classpart, true, classLoader);
-                        Method meth = null;
-                        Object instance = null;
-                        list.add(clazz);
-                        int last = methodTable.size()-1;
-                        int i=0;
-                        for(Enumeration en=methodTable.keys(); 
-                                                            en.hasMoreElements();) {
-                            String key = (String)en.nextElement();
-                            String[] paramparts = 
-                                getParamParts((String)methodTable.get(key));
-                            if(paramparts.length ==0) {
-                                paramTypes = new Class[0];
-                                paramArgs = new Object[0];
-                            } else {
-                                paramTypes = new Class[paramparts.length];
-                                for(int j=0; j<paramparts.length; j++)
-                                    paramTypes[j] = String.class;
-                                paramArgs = new Object[paramparts.length];
-                                System.arraycopy(paramparts,
-                                                 0,
-                                                 paramArgs,
-                                                 0,
-                                                 paramArgs.length);
-                            }
-                            meth = clazz.getMethod(key, paramTypes);
-                            list.add(meth);
-                            if(i < last) {
-                                clazz = meth.getReturnType();
-                                list.add(clazz);
-                            }
-                            i++;
-                        }
-                        Iterator iter = list.iterator();
-                        while(iter.hasNext()) {
-                            try {
-                                clazz = (Class)iter.next();
-                                meth = (Method)iter.next();
-                                instance = clazz.newInstance();
-                            } catch(Exception ignore) {
-                                // if no ctor, then it is assumed the method is static
-                            }
-                            instance = meth.invoke(instance, paramArgs);
-                        }
-                        if(instance==null) {
-                            isThrowing = true;
-                            System.out.println("Target ["+target+"] unresolvable");
-                            //return(null);
-                            throw new SAXException("Target ["+target+"] unresolvable");
-                        }
-                        return(new InputSource(
-                                      new StringReader(instance.toString().trim())));
-                    } catch(Throwable th) {
-                        if(isThrowing)
-                            throw (SAXException)th;
-                        th.printStackTrace();
-                        return(null);
-                    }
-                } else { // we have a java resource to load
-                    InputStream in = getClassLoader().getResourceAsStream(target);
-                    if(in != null)
-                        return(new InputSource(in));
-                    else {                        
-                        logger.log(Level.WARNING, 
-                                   "Failed to find in class path " + systemId);
-                        System.out.flush();
+        String systemId = systemIdentifier;
+        if(systemId == null) {
+            return null;
+        }
 
-                        return(null);
+        if(systemId.equals(PUBLIC_DTD))
+            systemId = "java://"+DTD;
+        if(systemId.startsWith("java://")) {
+            String target = systemId.substring(7);
+            if (OLD_DTD.equals(target))
+                target = DTD;
+            int paren;
+            if((paren = target.indexOf('(')) > 2) {
+                /* we have a java class to execute
+                 * the form foo.bar() is executed and the result is passed back
+                 * as a string */
+                int demarc = target.lastIndexOf('.', paren);
+                String classpart = target.substring(0, demarc);
+                Hashtable methodTable =
+                    getMethodParts(target.substring(demarc+1));
+                List list = new LinkedList();
+                Class[] paramTypes;
+                Object[] paramArgs=null;
+                try {
+                    Class clazz = Class.forName(classpart, true, classLoader);
+                    Method meth = null;
+                    Object instance = null;
+                    list.add(clazz);
+                    int last = methodTable.size()-1;
+                    int i=0;
+                    for(Enumeration en=methodTable.keys();
+                        en.hasMoreElements();) {
+                        String key = (String)en.nextElement();
+                        String[] paramParts = getParamParts((String)methodTable.get(key));
+                        if(paramParts.length ==0) {
+                            paramTypes = new Class[0];
+                            paramArgs = new Object[0];
+                        } else {
+                            paramTypes = new Class[paramParts.length];
+                            for(int j=0; j<paramParts.length; j++)
+                                paramTypes[j] = String.class;
+                            paramArgs = new Object[paramParts.length];
+                            System.arraycopy(paramParts, 0, paramArgs, 0, paramArgs.length);
+                        }
+                        meth = clazz.getMethod(key, paramTypes);
+                        list.add(meth);
+                        if(i < last) {
+                            clazz = meth.getReturnType();
+                            list.add(clazz);
+                        }
+                        i++;
                     }
+                    Iterator iter = list.iterator();
+                    while(iter.hasNext()) {
+                        try {
+                            clazz = (Class)iter.next();
+                            meth = (Method)iter.next();
+                            instance = clazz.newInstance();
+                        } catch(Exception ignore) {
+                            // if no ctor, then it is assumed the method is static
+                        }
+                        instance = meth.invoke(instance, paramArgs);
+                    }
+                    if(instance==null) {
+                        isThrowing = true;
+                        System.out.println("Target ["+target+"] unresolvable");
+                        //return(null);
+                        throw new SAXException("Target ["+target+"] unresolvable");
+                    }
+                    return(new InputSource(
+                                              new StringReader(instance.toString().trim())));
+                } catch(Throwable th) {
+                    if(isThrowing)
+                        throw (SAXException)th;
+                    th.printStackTrace();
+                    return(null);
                 }
-            } else {
-                // use default resolver
-                return(null);
+            } else { // we have a java resource to load
+                InputStream in = getClassLoader().getResourceAsStream(target);
+                if(in != null)
+                    return(new InputSource(in));
+                else {
+                    logger.log(Level.WARNING,
+                               "Failed to find in class path " + systemId);
+                    System.out.flush();
+
+                    return(null);
+                }
             }
-        } else
+        } else {
+            // use default resolver
             return(null);
+        }
+
     }
    
     /** 
@@ -176,14 +172,6 @@ public class JavaEntityResolver implements EntityResolver {
      */
     public ClassLoader getClassLoader() {
         return(classLoader);
-    }
-
-    /** 
-     * Setter for property classLoader.
-     * @param classLoader New value of property classLoader.
-     */
-    public void setClassLoader(ClassLoader classLoader) {
-        this.classLoader = classLoader;
     }
 
     private Hashtable getMethodParts(String target) {
