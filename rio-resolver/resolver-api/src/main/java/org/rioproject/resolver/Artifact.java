1fa651a7e695dae7968ebb093dbce8e8539cd57a
dreedyman <dennis.reedy@gmail.com>
2012-06-05 16:12:10 -0400

    Added equals and hashcode

--- rio-resolver/resolver-api/src/main/java/org/rioproject/resolver/Artifact.java
+++ rio-resolver/resolver-api/src/main/java/org/rioproject/resolver/Artifact.java
@@ -125,6 +125,34 @@ public class Artifact {
         return sb.toString();
     }
 
+    @Override
+    public boolean equals(Object o) {
+        if (this == o)
+            return true;
+        if (o == null || getClass() != o.getClass())
+            return false;
+
+        Artifact artifact = (Artifact) o;
+
+        if (artifactId != null ? !artifactId.equals(artifact.artifactId) : artifact.artifactId != null) return false;
+        if (classifier != null ? !classifier.equals(artifact.classifier) : artifact.classifier != null) return false;
+        if (groupId != null ? !groupId.equals(artifact.groupId) : artifact.groupId != null) return false;
+        if (type != null ? !type.equals(artifact.type) : artifact.type != null) return false;
+        if (version != null ? !version.equals(artifact.version) : artifact.version != null) return false;
+
+        return true;
+    }
+
+    @Override
+    public int hashCode() {
+        int result = artifactId != null ? artifactId.hashCode() : 0;
+        result = 31 * result + (groupId != null ? groupId.hashCode() : 0);
+        result = 31 * result + (version != null ? version.hashCode() : 0);
+        result = 31 * result + (classifier != null ? classifier.hashCode() : 0);
+        result = 31 * result + (type != null ? type.hashCode() : 0);
+        return result;
+    }
+
     /**
      * Check to see if the provided string represents an Artifact
      *
