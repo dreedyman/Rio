b1f0dfb271d3e969cb7c0d188a653d42fb74ccf5
dreedyman <dennis.reedy@gmail.com>
2012-11-08 08:24:33 -0500

    Parse groovy opstrings and create OperationaString object(s) directly, no longer support the building and parsing of XML documents.

--- rio-lib/src/main/groovy/org/rioproject/opstring/OpStringParserGlobals.groovy
+++ rio-lib/src/main/groovy/org/rioproject/opstring/OpStringParserGlobals.groovy
@@ -1,5 +1,5 @@
 /*
- * Copyright 2008 the original author or authors.
+ * Copyright to the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -13,18 +13,20 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package org.rioproject.opstring.handlers
+package org.rioproject.opstring
 
 /**
- * Handles the parsing for the MaxPerMachine element
+ * Constants used as labels for global parsed properties.
  *
- * @author Jerome Bernard
+ * @author Dennis Reedy
  */
-class MaxPerMachineHandler implements Handler {
-    public parse(Object element, Object options) {
-        def sDescriptor = options['serviceDescriptor']
-
-        sDescriptor.maxPerMachine = element.text()
-        sDescriptor.machineBoundary = element.'@type'
-    }
+public interface OpStringParserGlobals {
+    final String GROUPS = "groups"
+    final String LOCATORS = "locators"
+    final String ASSOCIATIONS = "associations"
+    final String CONFIGURATION = "configuration"
+    final String CODEBASE = "codebase"
+    final String CLUSTER = "cluster"
+    final String OPSTRING = "opstring"
+    final String LOGGERS = "loggers"
 }
