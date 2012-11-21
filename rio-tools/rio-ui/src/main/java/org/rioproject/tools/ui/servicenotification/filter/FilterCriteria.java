e4c74357da382624e897a692e7a82b9dcff9e361
dreedyman <dennis.reedy@gmail.com>
2012-11-21 11:58:15 -0500

    Override equals() and hashcode()

--- rio-tools/rio-ui/src/main/java/org/rioproject/tools/ui/servicenotification/filter/FilterCriteria.java
+++ rio-tools/rio-ui/src/main/java/org/rioproject/tools/ui/servicenotification/filter/FilterCriteria.java
@@ -24,28 +24,49 @@ import java.util.List;
  */
 public class FilterCriteria {
     private final java.util.List<String> eventTypes = new ArrayList<String>();
-    private final java.util.List<String> contains = new ArrayList<String>();
+    private final java.util.List<String> descriptions = new ArrayList<String>();
 
     public void addEventType(String eventType) {
         eventTypes.add(eventType);
     }
 
-    public void addContains(String contains) {
-        this.contains.add(contains);
+    public void addDescription(String contains) {
+        this.descriptions.add(contains);
     }
 
     public Collection<String> getEventTypes() {
         return eventTypes;
     }
 
-    public List<String> getContains() {
-        return contains;
+    public List<String> getDescriptions() {
+        return descriptions;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o)
+            return true;
+        if (o == null || getClass() != o.getClass())
+            return false;
+
+        FilterCriteria that = (FilterCriteria) o;
+
+        return !(descriptions != null ? !descriptions.equals(that.descriptions) : that.descriptions != null) &&
+               !(eventTypes != null ? !eventTypes.equals(that.eventTypes) : that.eventTypes != null);
+
+    }
+
+    @Override
+    public int hashCode() {
+        int result = eventTypes != null ? eventTypes.hashCode() : 0;
+        result = 31 * result + (descriptions != null ? descriptions.hashCode() : 0);
+        return result;
     }
 
     @Override
     public String toString() {
         return "FilterCriteria " +
                "eventTypes=" + eventTypes +
-               ", contains=" + contains;
+               ", descriptions=" + descriptions;
     }
 }
