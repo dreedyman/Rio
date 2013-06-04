744a262cbe1d45bb9ce5e02f89b7adbc316c4e09
dreedyman <dennis.reedy@gmail.com>
2013-06-04 11:39:51 -0400

    Version number and license headers

--- examples/tomcat/src/main/opstring/tomcat.groovy
+++ examples/tomcat/src/main/opstring/tomcat.groovy
@@ -1,11 +1,30 @@
+/*
+ * Copyright to the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 import org.rioproject.config.Constants
 
+/**
+ * The deployment configuration for the Tomcat example
+ *
+ * @author Dennis Reedy
+ */
 deployment(name: 'Tomcat Deploy') {
     /* Configuration for the discovery group that the service should join.
      * This first checks if the org.rioproject.groups property is set, if not
      * the user name is used */
-    groups System.getProperty(Constants.GROUPS_PROPERTY_NAME,
-                              System.getProperty('user.name'))
+    groups System.getProperty(Constants.GROUPS_PROPERTY_NAME, System.getProperty('user.name'))
 
     serviceExec(name: 'Tomcat') {
 
@@ -40,7 +59,6 @@ deployment(name: 'Tomcat Deploy') {
                     objectName: "Catalina:name=http-8080,type=ThreadPool",
                     attribute: 'currentThreadsBusy', period: 5000
         }
-
 
         maintain 1
     }
