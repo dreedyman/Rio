1285630230b1ec2c7475d66746e161125bbe2bd6
dreedyman <dennis.reedy@gmail.com>
2014-03-20 21:40:57 -0400

    Improve SystemComponent.toString()rio-core/rio-api/src/main/java/org/rioproject/deploy/SystemComponent.java

---
+++ rio-start/src/main/java/org/rioproject/config/RioProperties.java
@@ -0,0 +1,11 @@
+package org.rioproject.config;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: reedydg
+ * Date: 3/20/14
+ * Time: 4:09 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class RioProperties {
+}
---
+++ rio-start/src/test/java/org/rioproject/config/RioPropertiesTest.java
@@ -0,0 +1,17 @@
+package org.rioproject.config;
+
+import org.junit.Test;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: reedydg
+ * Date: 3/20/14
+ * Time: 5:11 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class RioPropertiesTest {
+    @Test
+    public void testLoad() throws Exception {
+
+    }
+}
