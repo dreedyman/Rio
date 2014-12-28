e9500cbc2f6fb18b0ab3498f7aee231b900ecc5c
John Sarman <johnsarman@gmail.com>
2014-12-28 07:53:13 -0500

    Updated example opstrings to use version 2.2.  Updated rio-ui to include rio-proxy during assembly of standalone jar.

--- examples/hospital/hospital-service/src/main/resources/HospitalConfig.groovy
+++ examples/hospital/hospital-service/src/main/resources/HospitalConfig.groovy
@@ -24,7 +24,7 @@ class HospitalConfig {
 
     Entry[] getServiceUIs(String codebase) {
         String uiClass = 'org.rioproject.examples.hospital.ui.HospitalIntro'
-        URL url = new URL("artifact:org.rioproject.examples.hospital:hospital-ui:2.1")
+        URL url = new URL("artifact:org.rioproject.examples.hospital:hospital-ui:2.2")
         def entry = [UIDescriptorFactory.getUIDescriptor(MainUI.ROLE, new UIComponentFactory(url, uiClass))]
         return entry as Entry[]
     }
