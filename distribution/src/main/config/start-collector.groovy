7736a26091182d3b0b88eeb261799fae551df8c1
dreedyman <dennis.reedy@gmail.com>
2015-07-07 12:20:42 -0400

    Roll version to 5.1.7

--- distribution/src/main/config/start-collector.groovy
+++ distribution/src/main/config/start-collector.groovy
@@ -41,7 +41,7 @@ class StartCollectorConfig {
         pathBuilder.append(rioHome).append(File.separator).append("lib").append(File.separator).append("event-collector-service.jar")
 
         def serviceDescriptors = [
-                new RioServiceDescriptor("artifact:org.rioproject.event-collector/event-collector-proxy/5.1.6",
+                new RioServiceDescriptor("artifact:org.rioproject.event-collector/event-collector-proxy/5.1.7",
                                          policyFile,
                                          pathBuilder.toString(),
                                          "org.rioproject.eventcollector.service.EventCollectorImpl",
--- rio-services/gnostic/src/main/opstring/gnostic.groovy
+++ rio-services/gnostic/src/main/opstring/gnostic.groovy
@@ -2,8 +2,8 @@
 deployment(name: 'Gnostic') {
     groups System.getProperty("org.rioproject.groups", System.getProperty('user.name'))
 
-    artifact id: 'service', 'org.rioproject.gnostic:gnostic-service:5.1.6'
-    artifact id: 'service-api', 'org.rioproject.gnostic:gnostic-api:5.1.6'
+    artifact id: 'service', 'org.rioproject.gnostic:gnostic-service:5.1.7'
+    artifact id: 'service-api', 'org.rioproject.gnostic:gnostic-api:5.1.7'
 
     service(name: 'Gnostic') {
         interfaces {
