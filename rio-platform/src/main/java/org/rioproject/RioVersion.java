f3f56d1c1ab6fe21c81b4af634c8793286cad8be
dreedyman <dennis.reedy@gmail.com>
2016-10-05 15:23:15 -0400

    Roll to version 5.5

--- rio-platform/src/main/java/org/rioproject/RioVersion.java
+++ rio-platform/src/main/java/org/rioproject/RioVersion.java
@@ -27,7 +27,7 @@ import java.net.URL;
  */
 public final class RioVersion {
     /** Current version of Rio */ 
-    public static final String VERSION = "5.4";
+    public static final String VERSION = "5.5";
 
     private RioVersion() {}
 
