73decab420ea406c915e14403d0942ac7f5635b2
Dennis Reedy <dennis.reedy@gmail.com>
2021-08-30 14:21:13 -0400

    Default https to false

--- rio-test/src/main/java/org/rioproject/test/RioTestConfig.java
+++ rio-test/src/main/java/org/rioproject/test/RioTestConfig.java
@@ -34,5 +34,5 @@ public @interface RioTestConfig {
     int numLookups() default 0;
     String opstring() default "";
     boolean autoDeploy() default true;
-    boolean https() default true;
+    boolean https() default false;
 }
