6f4397ffc783683013e80012da0d0f0ff9a0ccaa
Dennis Reedy <dennis.reedy@gmail.com>
2021-09-21 14:43:29 -0400

    Check if certificate is valid

--- rio-platform/src/test/java/org/rioproject/security/AggregateTrustManagerTest.java
+++ rio-platform/src/test/java/org/rioproject/security/AggregateTrustManagerTest.java
@@ -4,21 +4,18 @@ import org.junit.Before;
 import org.junit.Test;
 import org.rioproject.config.Constants;
 
-import javax.net.ssl.HostnameVerifier;
-import javax.net.ssl.HttpsURLConnection;
 import javax.net.ssl.SSLHandshakeException;
-import javax.net.ssl.SSLSession;
 import java.io.File;
-import java.net.URISyntaxException;
 import java.net.URL;
 import java.net.URLConnection;
 import java.security.KeyStore;
 
-import static org.junit.Assert.*;
+import static org.junit.Assume.assumeTrue;
 
 public class AggregateTrustManagerTest {
     private File keyStoreFile;
     private KeyStore keyStore;
+
     @Before
     public void setup() throws Exception {
         keyStoreFile = new File(System.getProperty(Constants.KEYSTORE));
@@ -27,6 +24,7 @@ public class AggregateTrustManagerTest {
 
     @Test
     public void testWithDefault_usingBothCacertsAndRioKeyStore() throws Exception {
+        assumeTrue(KeyStoreHelper.notExpired(keyStore, "rio-project"));
         AggregateTrustManager.initialize(keyStore);
         URL locationURL = new URL("https://www.google.com");
         URLConnection urlConnection = locationURL.openConnection();
@@ -35,6 +33,7 @@ public class AggregateTrustManagerTest {
 
     @Test(expected= SSLHandshakeException.class)
     public void testWithJustRioKeystore_shouldFail() throws Exception {
+        assumeTrue(KeyStoreHelper.notExpired(keyStore, "rio-project"));
         System.setProperty("javax.net.ssl.trustStore", keyStoreFile.getPath());
         AggregateTrustManager.initialize(keyStore);
         URL locationURL = new URL("https://www.google.com");
